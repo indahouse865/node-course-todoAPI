327f79a385166fc85f0a0bdfc4707f25898ec432
David <dme280@nyu.edu>
2018-07-17 01:04:21 -0400

    Add variable path for todo ids

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const {ObjectID} = require("mongodb");
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/todo");
 const {User} = require("./models/user");
@@ -26,6 +27,24 @@ app.get("/todos", (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get("/todos/:id", (req, res) => {
+    let id = req.params.id;
+    //validate id
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
