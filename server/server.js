45377f224ec739e91a553d0922048874bc8a150d
David <dme280@nyu.edu>
2018-07-31 01:38:33 -0400

    Add new GET path and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require("mongodb");
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/todo");
 const {User} = require("./models/user");
+const {authenticate} = require("./middleware/authenticate");
 
 let app = express();
 const port = process.env.PORT || 3000;
@@ -89,7 +90,7 @@ app.patch("/todos/:id", (req, res) => {
         if (!todo) {
             return res.status(404).send("404 - ID does not exist");
         }
-            
+
         res.send({todo});
     }).catch((e) => {
         res.status(400).send("400");
@@ -108,6 +109,10 @@ app.post("/users", (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
